1d7ac507776393b656a8a9feddea1ac3b8062fba
Clare Constantine <clare@code.org>
2022-04-24 20:24:31 -0700

    adjust csv parsing

--- bin/generate_schedule.rb
+++ bin/generate_schedule.rb
@@ -16,7 +16,6 @@ COMPANY_4 = 6
 COMPANY_5 = 7
 COMPANY_6 = 8
 
-
 data_by_time_block = {}
 
 CSV.foreach(source_data_csv, headers: true) do |row|
@@ -26,14 +25,11 @@ CSV.foreach(source_data_csv, headers: true) do |row|
     data_by_time_block[day] = {}
   end
 
-    # if we haven't seen this ampm value for this day yet, add it
+  # if we haven't seen this ampm value for this day yet, add it
   ampm = row[AMPM]
   if !data_by_time_block[day][ampm]
     data_by_time_block[day][ampm] = {}
   end
 
-  data_by_time_block[day][ampm] = {
-    mentor: row[MENTOR],
-    companies: [row[COMPANY_1], row[COMPANY_2], row[COMPANY_3], row[COMPANY_4], row[COMPANY_5], row[COMPANY_6]]
-  }
+  data_by_time_block[day][ampm][row[MENTOR]] = [row[COMPANY_1], row[COMPANY_2], row[COMPANY_3], row[COMPANY_4], row[COMPANY_5], row[COMPANY_6]]
 end
